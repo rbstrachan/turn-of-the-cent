5f79da898cc8916e73a533ca82aa4a869adcbab3
Horace Grindelwald <rossbstrachan@gmail.com>
2018-02-22 16:12:24 +0000

    Update turn_100.py
    Fixes #1

--- turn_100.py
+++ turn_100.py
@@ -8,12 +8,24 @@ currentYear = datetime.now().year
 go_again = True
 
 while go_again:
-	age = raw_input("How old are you? ")
-	age_100 = currentYear - int(age) + 100
+	input = raw_input("Please tell me the year you were born, or your age. ")
+	
+	if int(input) <= 99:
+		age_100 = currentYear - int(input) + 100
+	
+		print
+		print "You are " + str(input) + " years old right now. That means you'll turn 100 in the year " + str(age_100 - 1) + " or possibly " + str(age_100) + "."
+		print
+	elif int(input) >= 1900:
+		age_100 = int(input) + 100
 
-	print
-	print "You are " + str(age) + " years old right now. That means you'll turn 100 in the year " + str(age_100) + " or possibly " + str(age_100 + 1) + "."
-	print
+		print
+		print "You are " + str(currentYear - int(input)) + " years old right now. That means you'll turn 100 in the year " + str(age_100) + " or possibly " + str(age_100 + 1) + "."
+		print
+	else:
+		print
+		print "Sorry, that didn't work. Please enter the year you were born or your age."
+		print
 	
 	again = raw_input("Would you like to try again? ")
 	if again.lower() == "no" or again.lower() == "n":
